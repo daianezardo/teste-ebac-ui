acbc7a77cc2865db3f40ee4346c9617c0400a791
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2023-04-17 21:17:39 -0300

    Alterações nos testes automatizados

---
+++ cypress/e2e/produtos.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe('Funcionalidade Página de produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+        it('Deve selecionar um produto da lista', () => {
+            cy.get('[class="product-block grid"]').first().click()
+        });
+
+        it.only('Deve adicionar um produto no carrinho', () => {
+            cy.get('[class="product-block grid"]').first().click()
+            cy.get('.button-variable-item-XS').click()
+            cy.get('.button-variable-item-Red').click()
+            cy.get('.input-text').clear().type('2')
+            cy.get('.single_add_to_cart_button').click()
+
+            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 2)
+        });
+
+
+});
