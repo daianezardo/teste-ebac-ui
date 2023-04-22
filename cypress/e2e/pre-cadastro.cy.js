e51616ee432d552a4272c7018fcf352c2657afbd
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2023-04-22 12:17:19 -0300

    Usando massa de dados

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -4,7 +4,7 @@ import { faker } from '@faker-js/faker'
 describe('', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta')
     });
 
     it('Deve completar o pré-cadastro com sucesso', () => {
