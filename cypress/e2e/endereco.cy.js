e86c34e9e7b1f6a971e5a469e9180a33feb4c4b8
Daiane Zardo <daiane.abreu@al.infnet.edu.br>
2023-04-25 22:01:58 -0300

    Usando arquivo de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
+
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
@@ -9,6 +12,29 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
         
     });
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
+        enderecoPage.editarEnderecoFaturamento('Daiane', 'Zardo', 'Ebac', 'Brasil', 'Rua Romualdo de Barros', '250', 'Florianopolis', 'Santa Catarina', '88040410', '51988665522', 'teste@teste.com')
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
     });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[0].nome,
+            dadosEndereco[0].sobrenome,
+            dadosEndereco[0].empresa,
+            dadosEndereco[0].pais,
+            dadosEndereco[0].endereco,
+            dadosEndereco[0].numero,
+            dadosEndereco[0].cidade,
+            dadosEndereco[0].estado,
+            dadosEndereco[0].cep,
+            dadosEndereco[0].telefone,
+            dadosEndereco[0].email,
+            )
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
+    });
+    
 });
